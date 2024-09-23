44a40993c716ea1373544fcfb11a84ae6c9ab803
Thiago Razzini <razzini.thiago@gmail.com>
2024-09-23 07:52:39 -0300

    feat: Notificações com MediatR

--- DevFreela.Application/Commands/InsertProject/InsertProjectCommandHandler.cs
+++ DevFreela.Application/Commands/InsertProject/InsertProjectCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using DevFreela.Application.Models;
+using DevFreela.Application.Notification.ProjectCreated;
 using DevFreela.Infrastructure.Persistence;
 using MediatR;
 
@@ -7,10 +8,12 @@ namespace DevFreela.Application.Commands.InsertProject
     public class InsertProjectCommandHandler : IRequestHandler<InsertProjectCommand, ResultViewModel<int>>
     {
         private readonly DevFreelaDbContext _context;
+        private readonly IMediator _mediator;
 
-        public InsertProjectCommandHandler(DevFreelaDbContext context)
+        public InsertProjectCommandHandler(DevFreelaDbContext context, IMediator mediator)
         {
             _context = context;
+            _mediator = mediator;
         }
 
         public async Task<ResultViewModel<int>> Handle(InsertProjectCommand request, CancellationToken cancellationToken)
@@ -19,6 +22,9 @@ namespace DevFreela.Application.Commands.InsertProject
 
             await _context.Projects.AddAsync(project);
             await _context.SaveChangesAsync();
+
+            var projectCreated = new ProjectCreatedNotification(project.Id, project.Title, project.TotalCost);
+            await _mediator.Publish(projectCreated);
 
             return ResultViewModel<int>.Success(project.Id);
         }
